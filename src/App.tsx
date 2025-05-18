fe9fe8a057803f3646319b4a83416a4a35b1737f
Mange Jay <jaymange263@gmail.com>
2025-05-18 17:36:20 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* NAV */}
+      <div className='flex m-5'>
+        
+        
+        {/* Left */}
+        <div className='w-full'>
+          <p>Mange Jay</p>
+          </div>
+        {/* Left */}
+
+        {/* Right */}
+        <div className='flex w-full space-x-7 justify-end'> 
+          <div>Home</div>
+          <div>About</div>
+          <div>Contact</div>
+          <div>Projects</div>
+        </div>
+        {/* Right */}
+
+      </div>
+    {/* NAV */}
+    </>
+  )
+}
+
+export default App
